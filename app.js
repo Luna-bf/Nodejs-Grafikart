f5967d004bad3d54a9c7596b15f0e350f0ed02e7
Luna-bf <lburattifayolle@gmail.com>
2025-04-07 08:52:59 +0200

    Added comments

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import fs from 'node:fs'
 
-const content = fs.readFileSync('demo.txt', {encoding: 'utf-8'});
+/*Ici je demande à la variable content de lire le fichier 'demo.txt'*/
+const content = fs.readFileSync('demo.txt', {encoding: 'utf-8'}); /*De base, la console affiche le contenu sous format buffer,
+                                                                  je lui demande donc de l'encoder en format utf-8*/
 
-console.log(content);
+console.log(content); /*Je fais un console.log de la variable content pour afficher son contenu dans la console*/
